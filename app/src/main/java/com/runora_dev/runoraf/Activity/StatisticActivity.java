151ff1058cb0d72bc0679bf60a1730ef6cbd7491
ahmed <acc.ahmedstudio@gmail.com>
2023-07-30 23:28:40 +0400

    change the pie to linear

--- app/src/main/java/com/runora_dev/runoraf/Activity/StatisticActivity.java
+++ app/src/main/java/com/runora_dev/runoraf/Activity/StatisticActivity.java
@@ -3,8 +3,11 @@ package com.runora_dev.runoraf.Activity;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.github.mikephil.charting.charts.LineChart;
 import com.github.mikephil.charting.charts.PieChart;
@@ -27,7 +30,7 @@ import java.util.List;
 
 public class StatisticActivity extends AppCompatActivity {
 
-    private PieChart pieChart;
+    private LineChart pieChart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,7 +39,19 @@ public class StatisticActivity extends AppCompatActivity {
         LineChart lineChart = findViewById(R.id.lineChartBurned);
         lineChart.setDragEnabled(true);
         lineChart.setScaleEnabled(false);
-        pieChart = (PieChart) findViewById(R.id.pieChart);
+
+        pieChart = (LineChart) findViewById(R.id.pieChart);
+        pieChart.setDragEnabled(true);
+        pieChart.setScaleEnabled(false);
+
+        ImageView statisticBack = findViewById(R.id.statisc_back);
+        statisticBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               Intent intent = new Intent(getBaseContext(), Home.class);
+               startActivity(intent);
+            }
+        });
 // Get a reference to the Firebase Realtime Database
         DatabaseReference databaseRef = FirebaseDatabase.getInstance().getReference("dailyFood");
 
@@ -75,15 +90,6 @@ public class StatisticActivity extends AppCompatActivity {
                 PieData pieData = new PieData(dataSet);
                 pieData.setValueTextSize(12f);
 
-//                // Set additional properties of the chart
-//                pieChartConsumedFood.setData(pieData);
-//                pieChartConsumedFood.getDescription().setEnabled(false);
-//                pieChartConsumedFood.setDrawEntryLabels(false);
-//                pieChartConsumedFood.setHoleColor(Color.TRANSPARENT);
-//                pieChartConsumedFood.setTransparentCircleRadius(0f);
-//
-//                // Refresh the chart
-//                pieChartConsumedFood.invalidate();
             }
 
             @Override
@@ -91,54 +97,116 @@ public class StatisticActivity extends AppCompatActivity {
                 // Handle any errors
             }
         });
-        // Create entries for the graph
-        List<PieEntry> entries = new ArrayList<>();
-        entries.add(new PieEntry(10, "Last Week"));
-        entries.add(new PieEntry(8, "Last Week"));
-        entries.add(new PieEntry(15, "Last Month"));
 
-        // Create a dataset with the entries
-        PieDataSet dataSet = new PieDataSet(entries, "");
+//        // Create entries for the graph
+//        List<PieEntry> entries = new ArrayList<>();
+//        entries.add(new PieEntry(10, "Last Week"));
+//        entries.add(new PieEntry(8, "Last Week"));
+//        entries.add(new PieEntry(15, "Last Month"));
+//
+//        // Create a dataset with the entries
+//        PieDataSet dataSet = new PieDataSet(entries, "");
+//
+//        // Set colors for the segments
+//        dataSet.setColors(Color.BLUE, Color.GREEN, Color.RED);
+//
+//        // Create a PieData object and add the dataset to it
+//        PieData pieData = new PieData(dataSet);
+//        pieData.setValueTextSize(12f);
+//
+//        // Set additional properties of the chart
+//        pieChart.setData(pieData);
+//        pieChart.getDescription().setEnabled(false);
+//        pieChart.setDrawEntryLabels(false);
+//        pieChart.setHoleColor(Color.TRANSPARENT);
+//        pieChart.setTransparentCircleRadius(0f);
+//
+//        // Refresh the chart
+//        pieChart.invalidate();
 
-        // Set colors for the segments
-        dataSet.setColors(Color.BLUE, Color.GREEN, Color.RED);
+        // Dummy food consumption data for the last 30 days
+        float[] foodburnedLastWeek = {4.5f, 3.0f, 2.0f, 2.5f, 3.5f, 2.0f, 6.5f, 3.0f, 2.5f, 3.0f,
+                3.5f, 4.0f, 2.5f, 2.0f, 2.5f, 5.0f, 9.5f, 4.0f, 3.6f, 3.0f};
 
-        // Create a PieData object and add the dataset to it
-        PieData pieData = new PieData(dataSet);
-        pieData.setValueTextSize(12f);
+        float[] foodburnedLastMonth = {6.5f, 5.0f, 7.0f, 5.5f, 4.5f, 6.0f, 4.5f, 5.0f, 6.5f, 4.0f,
+                3.5f, 4.0f, 5.5f, 6.0f, 5.5f, 6.0f, 5.5f, 5.0f, 6.0f, 7.0f, 5.5f, 6.0f, 7.0f};
 
-        // Set additional properties of the chart
-        pieChart.setData(pieData);
-        pieChart.getDescription().setEnabled(false);
-        pieChart.setDrawEntryLabels(false);
-        pieChart.setHoleColor(Color.TRANSPARENT);
-        pieChart.setTransparentCircleRadius(0f);
+        float[] foodburnedLastYear = {8.5f, 9.0f, 7.5f, 8.0f, 9.5f, 8.0f, 8.5f, 9.0f, 7.0f, 8.0f,
+                9.5f, 8.0f, 8.5f, 9.0f, 7.5f, 8.0f, 9.5f, 8.0f, 8.5f, 9.0f, 7.5f, 8.0f, 9.5f};
 
-        // Refresh the chart
+        ArrayList<Entry> entriesLastWeek = new ArrayList<>();
+        for (int i = 0; i < foodburnedLastWeek.length; i++) {
+            entriesLastWeek.add(new Entry(i, foodburnedLastWeek[i]));
+        }
+
+        ArrayList<Entry> entriesLastMonth = new ArrayList<>();
+        for (int i = 0; i < foodburnedLastMonth.length; i++) {
+            entriesLastMonth.add(new Entry(i, foodburnedLastMonth[i]));
+        }
+
+        ArrayList<Entry> entriesLastYear = new ArrayList<>();
+        for (int i = 0; i < foodburnedLastYear.length; i++) {
+            entriesLastYear.add(new Entry(i, foodburnedLastYear[i]));
+        }
+
+        LineDataSet dataSetLastWeek = new LineDataSet(entriesLastWeek, "Last Week");
+        dataSetLastWeek.setColor(Color.BLUE);
+        dataSetLastWeek.setValueTextColor(Color.BLACK);
+
+        LineDataSet dataSetLastMonth = new LineDataSet(entriesLastMonth, "Last Month");
+        dataSetLastMonth.setColor(Color.GREEN);
+        dataSetLastMonth.setValueTextColor(Color.BLACK);
+
+        LineDataSet dataSetLastYear = new LineDataSet(entriesLastYear, "Last Year");
+        dataSetLastYear.setColor(Color.RED);
+        dataSetLastYear.setValueTextColor(Color.BLACK);
+
+        ArrayList<ILineDataSet> dataSets = new ArrayList<>();
+        dataSets.add(dataSetLastWeek);
+        dataSets.add(dataSetLastMonth);
+        dataSets.add(dataSetLastYear);
+
+        LineData lineData = new LineData(dataSets);
+
+        pieChart.setData(lineData);
         pieChart.invalidate();
 
 
         // Dummy food consumption data for the last 30 days
-        float[] foodConsumption = {2.5f, 3.0f, 2.0f, 2.5f, 3.5f, 4.0f, 3.5f, 3.0f, 2.5f, 3.0f,
+        float[] foodConsumption1 = {2.5f, 3.0f, 2.0f, 2.5f, 3.5f, 4.0f, 3.5f, 3.0f, 2.5f, 3.0f,
                 3.5f, 3.0f, 2.5f, 2.0f, 2.5f, 3.0f, 3.5f, 4.0f, 3.5f, 3.0f,
                 2.5f, 3.0f, 3.5f, 3.0f, 2.5f, 2.0f, 2.5f, 3.0f, 3.5f, 4.0f};
 
-        ArrayList<Entry> entriess = new ArrayList<>();
-        for (int i = 0; i < foodConsumption.length; i++) {
-            entriess.add(new Entry(i, foodConsumption[i]));
+        ArrayList<Entry> entriess1 = new ArrayList<>();
+        for (int i = 0; i < foodConsumption1.length; i++) {
+            entriess1.add(new Entry(i, foodConsumption1[i]));
         }
 
-        LineDataSet dataSets = new LineDataSet(entriess, "Food Consumption");
-        dataSets.setColor(Color.BLUE);
-        dataSets.setValueTextColor(Color.BLACK);
+        LineDataSet dataSets2 = new LineDataSet(entriess1, "Food Consumption");
+        dataSets2.setColor(Color.BLUE);
+        dataSets2.setValueTextColor(Color.BLACK);
 
-        ArrayList<ILineDataSet> dataSetsd = new ArrayList<>();
-        dataSetsd.add(dataSets);
+        ArrayList<ILineDataSet> dataSetsd2 = new ArrayList<>();
+        dataSetsd2.add(dataSets2);
 
-        LineData lineData = new LineData(dataSets);
+        LineData lineData1 = new LineData(dataSetsd2);
 
-        lineChart.setData(lineData);
+        lineChart.setData(lineData1);
         lineChart.invalidate();
 
     }
+
+    @Override
+    public boolean onNavigateUp() {
+        onBackPressed();
+        return  true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        Intent intent = new Intent(Intent.ACTION_MAIN);
+        intent.addCategory(Intent.CATEGORY_HOME);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+        finish();    }
 }
