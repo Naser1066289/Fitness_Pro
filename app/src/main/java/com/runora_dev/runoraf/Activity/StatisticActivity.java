875b995cdd9a4dd9fe2e97084257dcf8089914a7
ahmed <acc.ahmedstudio@gmail.com>
2023-08-21 01:23:43 +0400

    diplay data based on week, month, year

--- app/src/main/java/com/runora_dev/runoraf/Activity/StatisticActivity.java
+++ app/src/main/java/com/runora_dev/runoraf/Activity/StatisticActivity.java
@@ -14,12 +14,14 @@ import android.widget.Spinner;
 
 import com.github.mikephil.charting.charts.LineChart;
 import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.components.XAxis;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
 import com.github.mikephil.charting.data.PieData;
 import com.github.mikephil.charting.data.PieDataSet;
 import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.formatter.IndexAxisValueFormatter;
 import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -29,7 +31,9 @@ import com.google.firebase.database.ValueEventListener;
 import com.runora_dev.runoraf.R;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class StatisticActivity extends AppCompatActivity {
 
@@ -219,6 +223,11 @@ public class StatisticActivity extends AppCompatActivity {
     private void updateChart(int position) {
         ArrayList<ILineDataSet> dataSets = new ArrayList<>();
         LineDataSet dataSet = null;
+        String[] xAxisLabels = null;
+        ArrayList<Entry> entries = new ArrayList<>();
+        float[] caloriesData = null;
+
+
         // Dummy food consumption data for the last 30 days
         float[] foodburnedLastWeek = {4.5f, 3.0f, 2.0f, 2.5f, 3.5f, 2.0f, 6.5f, 3.0f, 2.5f, 3.0f,
                 3.5f, 4.0f, 2.5f, 2.0f, 2.5f, 5.0f, 9.5f, 4.0f, 3.6f, 3.0f};
@@ -246,28 +255,102 @@ public class StatisticActivity extends AppCompatActivity {
 
         switch (position) {
             case 0: // Last Week
-                dataSet = new LineDataSet(entriesLastWeek, "Last Week");
-                dataSet.setColor(Color.BLUE);
-                dataSet.setValueTextColor(Color.BLACK);
+                xAxisLabels = new String[]{"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"};
+                caloriesData = new float[]{4.5f, 3.0f, 2.0f, 2.5f, 3.5f, 2.0f, 6.5f};
+                for (int i = 0; i < caloriesData.length; i++) {
+                    entries.add(new Entry(i, caloriesData[i]));
+                }
                 break;
             case 1: // Last Month
-                dataSet = new LineDataSet(entriesLastMonth, "Last Month");
-                dataSet.setColor(Color.GREEN);
-                dataSet.setValueTextColor(Color.BLACK);
+                String[] weeksOfMonth = {"Week 1", "Week 2", "Week 3", "Week 4"};
+                float[] totalCaloriesWeeks = new float[weeksOfMonth.length];
+
+                for (int i = 0; i < weeksOfMonth.length; i++) {
+                    totalCaloriesWeeks[i] = calculateTotalCaloriesForWeek(weeksOfMonth[i]);
+                    entries.add(new Entry(i, totalCaloriesWeeks[i]));
+                }
+                xAxisLabels = weeksOfMonth;
                 break;
             case 2: // Last Year
-                dataSet = new LineDataSet(entriesLastYear, "Last Year");
-                dataSet.setColor(Color.RED);
-                dataSet.setValueTextColor(Color.BLACK);
+                String[] monthsOfYear = {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
+                xAxisLabels = monthsOfYear; // Set the xAxisLabels for the year
+                float[] totalCaloriesMonths = new float[monthsOfYear.length];
+
+                for (int i = 0; i < monthsOfYear.length; i++) {
+                    totalCaloriesMonths[i] = calculateTotalCaloriesForMonth(monthsOfYear[i]);
+//                    float totalCalories = calculateTotalCaloriesForMonth(monthsOfYear[i]);
+                    entries.add(new Entry(i, totalCaloriesMonths[i]));
+                }
                 break;
         }
 
-        if (dataSet != null) {
-            dataSets.add(dataSet);
-            LineData lineData = new LineData(dataSets);
-            pieChart.setData(lineData);
-            pieChart.invalidate();
+        dataSet = new LineDataSet(entries, ""); // Create the dataset
+
+        // Set line color and value text color
+        dataSet.setColor(Color.BLUE);
+        dataSet.setValueTextColor(Color.BLACK);
+
+        dataSets.add(dataSet); // Add the dataset to the dataSets list
+
+        LineData lineData = new LineData(dataSets); // Create a LineData object with the dataSets list
+
+        // Set the data to the lineChart and invalidate to refresh the chart
+        pieChart.setData(lineData);
+        pieChart.invalidate();
+
+        // Set the x-axis labels using IAxisValueFormatter
+        XAxis xAxis = pieChart.getXAxis();
+        xAxis.setValueFormatter(new IndexAxisValueFormatter(xAxisLabels));
+        // Adjust the viewport for the x-axis
+        xAxis.setGranularity(1f); // This ensures that each label is shown
+        xAxis.setLabelCount(xAxisLabels.length); // Show all labels
+        xAxis.setAvoidFirstLastClipping(true); // Avoid clipping the first and last labels
+        xAxis.setLabelRotationAngle(45f); // Rotate the labels for better visibility
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM); // Position the labels at the bottom of the chart
+
+    }
+
+    private float calculateTotalCaloriesForMonth(String monthLabel) {
+        float totalCalories = 0;
+
+        // Define the data for each month as needed
+        // You can replace this with your actual data or modify the structure as required
+        Map<String, float[]> monthlyCaloriesData = new HashMap<>();
+        monthlyCaloriesData.put("Jan", new float[]{4.5f, 3.0f, 4.0f, 2.5f, 3.5f, 2.0f, 6.5f, 3.0f, 2.5f, 3.0f,
+                3.5f}); // Add actual data for January
+        monthlyCaloriesData.put("Feb", new float[]{9.5f, 6.0f, 7.0f, 2.5f, 3.5f, 2.0f, 6.5f, 3.0f, 2.5f, 3.0f,
+                3.5f});
+        monthlyCaloriesData.put("Mar", new float[]{0.5f, 5.0f, 3.0f, 2.5f, 3.5f, 2.0f, 6.5f, 3.0f, 2.5f, 3.0f,
+                3.5f});
+        // Add actual data for February
+        // Add more months and their data
+
+        float[] monthData = monthlyCaloriesData.get(monthLabel);
+        if (monthData != null) {
+            for (float calories : monthData) {
+                totalCalories += calories;
+            }
         }
+
+        return totalCalories;
+    }
+    private float calculateTotalCaloriesForWeek(String weekLabel) {
+        float totalCalories = 0;
+
+        // Define the data for each week as needed
+        // You can replace this with your actual data or modify the structure as required
+        Map<String, float[]> weeklyCaloriesData = new HashMap<>();
+        weeklyCaloriesData.put("Week 1", new float[]{4.5f, 3.0f, 2.0f, 2.5f, 3.5f, 2.0f, 6.5f});
+        // Add more weeks and their data
+
+        float[] weekData = weeklyCaloriesData.get(weekLabel);
+        if (weekData != null) {
+            for (float calories : weekData) {
+                totalCalories += calories;
+            }
+        }
+
+        return totalCalories;
     }
 
     @Override
