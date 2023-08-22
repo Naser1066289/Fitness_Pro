3286336e85f743f78ae473424c46904f82e40e71
ahmed <acc.ahmedstudio@gmail.com>
2023-08-22 22:00:11 +0400

    get profile data

--- app/src/main/java/com/runora_dev/runoraf/Activity/ProfileActivity.java
+++ app/src/main/java/com/runora_dev/runoraf/Activity/ProfileActivity.java
@@ -14,6 +14,11 @@ import com.github.mikephil.charting.charts.LineChart;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.runora_dev.runoraf.R;
@@ -26,9 +31,8 @@ public class ProfileActivity extends AppCompatActivity {
     private TextView emailTextView;
     private TextView phoneTextView;
 
-    private FirebaseFirestore firestore;
-    private FirebaseAuth firebaseAuth;
-
+    private FirebaseAuth mAuth;
+    private FirebaseDatabase mDatabase;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.profile);
@@ -36,44 +40,33 @@ public class ProfileActivity extends AppCompatActivity {
         emailTextView = findViewById(R.id.emailTextView);
         phoneTextView = findViewById(R.id.phoneTextView);
 
-        firestore = FirebaseFirestore.getInstance();
-        firebaseAuth = FirebaseAuth.getInstance();
+        mAuth = FirebaseAuth.getInstance();
+        mDatabase = FirebaseDatabase.getInstance();
 
-        // Get the current user's UID
-        String currentUserUid = firebaseAuth.getCurrentUser().getUid();
-        ImageView statisticBack = findViewById(R.id.profile_back_btn);
-        statisticBack.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getBaseContext(), Home.class);
-                startActivity(intent);
-            }
-        });
-        // Fetch user data from Firestore based on UID
-        firestore.collection("user").document(currentUserUid)
-                .get()
-                .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
-                    @Override
-                    public void onSuccess(DocumentSnapshot documentSnapshot) {
-                        if (documentSnapshot.exists()) {
-                            String name = documentSnapshot.getString("name");
-                            String email = documentSnapshot.getString("email");
-                            String phone = documentSnapshot.getString("phone");
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if (currentUser != null) {
+            String userId = currentUser.getUid();
+            mDatabase.getReference("user").child(userId).addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                    if (snapshot.exists()) {
+                        String name = snapshot.child("name").getValue(String.class);
+                        String email = snapshot.child("email").getValue(String.class);
+                        String phone = snapshot.child("phone").getValue(String.class);
 
-                            // Set the retrieved data to the UI elements
-                            nameTextView.setText(name);
-                            emailTextView.setText(email);
-                            phoneTextView.setText(phone);
-                        }
+                        nameTextView.setText(name);
+                        emailTextView.setText(email);
+                        phoneTextView.setText(phone);
                     }
-                })
-                .addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        // Handle error
-                    }
-                });
+                }
 
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+                    // Handle error
+                }
+            });
+
+        }
         LinearLayout editProfile = (LinearLayout) findViewById(R.id.btn);
         editProfile.setOnClickListener(new View.OnClickListener() {
             @Override
