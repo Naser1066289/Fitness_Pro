8c75abdf7be16d4c92d3de64dab93db4b1742098
ahmed <acc.ahmedstudio@gmail.com>
2023-08-24 19:37:57 +0400

    get user profile data and update

--- app/src/main/java/com/runora_dev/runoraf/Activity/EditProfileActivity.java
+++ app/src/main/java/com/runora_dev/runoraf/Activity/EditProfileActivity.java
@@ -3,14 +3,76 @@ package com.runora_dev.runoraf.Activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.api.Context;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.runora_dev.runoraf.R;
 
-public class EditProfileActivity extends AppCompatActivity {
+import org.checkerframework.checker.nullness.qual.NonNull;
 
+public class EditProfileActivity extends AppCompatActivity {
+    private EditText nameEditText;
+    private EditText emailEditText;
+    private EditText phoneEditText;
+    private Button saveButton;
+
+    private FirebaseFirestore firebaseFirestore;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_profile);
+    nameEditText = findViewById(R.id.nameEditTextEmail);
+    emailEditText = findViewById(R.id.emailTextEmail);
+    phoneEditText = findViewById(R.id.editTextPhoneNumber);
+    saveButton = findViewById(R.id.saveProfile);
+        String name = getIntent().getStringExtra("name");
+        String email = getIntent().getStringExtra("email");
+        String phone = getIntent().getStringExtra("phone");
+
+        nameEditText.setText(name);
+        emailEditText.setText(email);
+        phoneEditText.setText(phone);
+
+    firebaseFirestore = FirebaseFirestore.getInstance();
+
+    // Retrieve user data from previous activity or Firestore
+
+        saveButton.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            String newName = nameEditText.getText().toString();
+            String newEmail = emailEditText.getText().toString();
+            String newPhone = phoneEditText.getText().toString();
+
+            updateProfile(newName, newEmail, newPhone);
+        }
+    });
+}
+
+    private void updateProfile(String newName, String newEmail, String newPhone) {
+        String userId = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        DocumentReference userDocRef = firebaseFirestore.collection("user").document(userId);
+
+        userDocRef.update("name", newName, "email", newEmail, "phone", newPhone)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            // Update successful
+                            Toast.makeText(EditProfileActivity.this,"Your information updated successfully ",Toast.LENGTH_SHORT).show();
+                            finish(); // Finish the activity after successful update
+                        } else {
+                            // Handle update error
+                        }
+                    }
+                });
     }
 }
--- app/src/main/java/com/runora_dev/runoraf/Activity/ProfileActivity.java
+++ app/src/main/java/com/runora_dev/runoraf/Activity/ProfileActivity.java
@@ -1,8 +1,12 @@
 package com.runora_dev.runoraf.Activity;
 
 
+import static com.runora_dev.runoraf.Webservice.DatabaseHelper.name;
+
 import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -11,14 +15,17 @@ import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.github.mikephil.charting.charts.LineChart;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.runora_dev.runoraf.R;
@@ -27,54 +34,96 @@ import org.checkerframework.checker.nullness.qual.NonNull;
 
 
 public class ProfileActivity extends AppCompatActivity {
-    private TextView nameTextView;
-    private TextView emailTextView;
+    private TextView userNameTextView;
+    private TextView userEmailTextView;
     private TextView phoneTextView;
 
-    private FirebaseAuth mAuth;
-    private FirebaseDatabase mDatabase;
+    private FirebaseAuth firebaseAuth;
+    private FirebaseFirestore firebaseFirestore;
+    private ImageView profile_back_btn;
+    String userDataName, userDataEmail, userDataPhone;
+private  LinearLayout editButton;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.profile);
-        nameTextView = findViewById(R.id.nameTextView);
-        emailTextView = findViewById(R.id.emailTextView);
-        phoneTextView = findViewById(R.id.phoneTextView);
 
-        mAuth = FirebaseAuth.getInstance();
-        mDatabase = FirebaseDatabase.getInstance();
 
-        FirebaseUser currentUser = mAuth.getCurrentUser();
-        if (currentUser != null) {
-            String userId = currentUser.getUid();
-            mDatabase.getReference("user").child(userId).addListenerForSingleValueEvent(new ValueEventListener() {
-                @Override
-                public void onDataChange(@NonNull DataSnapshot snapshot) {
-                    if (snapshot.exists()) {
-                        String name = snapshot.child("name").getValue(String.class);
-                        String email = snapshot.child("email").getValue(String.class);
-                        String phone = snapshot.child("phone").getValue(String.class);
+        profile_back_btn = (ImageView) findViewById(R.id.profile_back_btn);
 
-                        nameTextView.setText(name);
-                        emailTextView.setText(email);
-                        phoneTextView.setText(phone);
-                    }
-                }
-
-                @Override
-                public void onCancelled(@NonNull DatabaseError error) {
-                    // Handle error
-                }
-            });
-
-        }
-        LinearLayout editProfile = (LinearLayout) findViewById(R.id.btn);
-        editProfile.setOnClickListener(new View.OnClickListener() {
+        profile_back_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getBaseContext(), EditProfileActivity.class);
+                Intent intent = new Intent(ProfileActivity.this, Home.class);
+                 startActivity(intent);
+            }
+        });
+        editButton = (LinearLayout) findViewById(R.id.btn);
+        editButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ProfileActivity.this, EditProfileActivity.class);
+                intent.putExtra("name", userDataName);
+                intent.putExtra("email", userDataEmail);
+                intent.putExtra("phone", userDataPhone);
                 startActivity(intent);
             }
         });
+        userNameTextView = findViewById(R.id.nameTextView);
+        userEmailTextView = findViewById(R.id.emailTextView);
+        phoneTextView = findViewById(R.id.phoneTextView);
+
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseFirestore = FirebaseFirestore.getInstance();
+
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+        Log.d("ProfileActivity : ", "User data "+user.toString() );
+
+        if (user != null) {
+            Log.d("ProfileActivity : ", "User is not null"+user.toString() );
+
+            String userEmail = user.getEmail();
+            String uid = user.getUid();
+            userEmailTextView.setText(userEmail);
+
+
+            DocumentReference userDocRef = firebaseFirestore.collection("user").document(uid);
+            userDocRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                @Override
+                public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                    if (task.isSuccessful()) {
+                        Log.d("ProfileActivity : ", "task is successfull "+task.isSuccessful() );
+
+                        DocumentSnapshot document = task.getResult();
+                        Log.d("ProfileActivity : ", "document data "+document );
+
+                        if (document.exists()) {
+                            Log.d("ProfileActivity : ", "document data exist "+document.toString() );
+
+                            String name = document.getString("name");
+                            String height = document.getString("height");
+                            String weight = document.getString("weight");
+                            String age = document.getString("age");
+                            String email = document.getString("email");
+                            String phone = document.getString("phone");
+                            userDataName = name;
+                            userDataEmail = email;
+                            userDataPhone = phone;
+
+                            userNameTextView.setText(name);
+                            userEmailTextView.setText(email);
+//                            userEmailTextView.setText(email);
+
+                            // Display additional info as needed
+                            String additionalInfo = "Age: " + age + "\nWeight: " + weight + "\nHeight: " + height;
+                            phoneTextView.setText(phone);
+                        }
+                    } else {
+                        Log.d("ProfileActivity", "Error getting user document: ", task.getException());
+                    }
+                }
+            });
+        }
     }
 
 }
